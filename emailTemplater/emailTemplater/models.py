1c77a7fb16e096f7c71181d466647743e6f5ed4f
haruna <haruna.kawakami867@gmail.com>
2023-01-20 22:54:10 +0900

    feat: add field to class

---
+++ emailTemplater/emailTemplater/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Template(models.Model):
+  id = models.AutoField(primary_key=True)
+  subject = models.CharField(max_length=200)
+  body = models.CharField(max_length=5000, blank=True, null=True)
+  show_in_list = models.BooleanField(default=True)
