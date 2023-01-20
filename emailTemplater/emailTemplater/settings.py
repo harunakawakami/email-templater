ba07776b126f38f1ccb0b082fff19d3bd1d38fa6
haruna <haruna.kawakami867@gmail.com>
2023-01-20 18:40:44 +0900

    feat: setup .env file

--- emailTemplater/emailTemplater/settings.py
+++ emailTemplater/emailTemplater/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*32e-1ao)jtdj_kh!b34l9gyamri5$ixfq_*n*kn(96s(3w-mg'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
