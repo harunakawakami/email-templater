6942fa4d0557eb10a9bb588c11a283da6e8444f7
haruna <haruna.kawakami867@gmail.com>
2023-01-21 21:19:48 +0900

    fix: start over django

--- emailTemplater/emailTemplater/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TemplateForm
-
-class TemplateFormSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = TemplateForm
-    fields = ['id', 'subject', 'body', 'show_in_list', 'tags']
--- emailTemplater/emailTemplater/settings.py
+++ emailTemplater/emailTemplater/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') 
+SECRET_KEY = 'django-insecure-s1e06vf*@!20=_copii($osd3b^y+pt=@y-2b3^arz*f%0-6-k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'emailTemplater',
-    'taggit',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
