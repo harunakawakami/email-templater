29a6ec3e15c9621a54929bd55a85489c9527c508
haruna <haruna.kawakami867@gmail.com>
2023-01-21 22:40:38 +0900

    fix: redo file structures, migtations

--- emailTemplater/emailTemplater/settings.py
+++ emailTemplater/emailTemplater/settings.py
@@ -10,17 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv() 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s1e06vf*@!20=_copii($osd3b^y+pt=@y-2b3^arz*f%0-6-k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+    'rest_framework',
+    'template'
 ]
 
 MIDDLEWARE = [
--- emailTemplater/template/models.py
+++ emailTemplater/template/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Template(models.Model):
+  subject = models.CharField(max_length=100)
+  body = models.TextField(blank=True)
+  show_in_list = models.BooleanField(help_text='Yes = show the template in the list')
+  tags = TaggableManager()
